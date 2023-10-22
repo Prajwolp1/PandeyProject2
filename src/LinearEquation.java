14d943165a2205c88ba838bd0ea8b75b044869ae
prajwolp@nycstudents.net <PrajwolPandey>
2023-10-22 11:55:09 -0700

    Commit 2

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class LinearEquation {
+public class LinearEquation {
+    int xOne;
+    int xTwo;
+    int yOne;
+    int yTwo;
+    int deltaY;
+    int deltaX;
+    String slope;
+    double b;
+    double xEquation;
+
+    public LinearEquation(int x1, int x2, int y1, int y2)   {
+        xOne = x1;
+        xTwo = x2;
+        yOne = y1;
+        yTwo = y2;
+    }
+    public String slopes() {
+        deltaY = yTwo - yOne;
+        deltaX = xTwo - xOne;
+        return deltaY + "/"+ deltaX + "x";
+    }
+    public double getDoubleSlope()    {
+        double intSlope = (double)deltaY / deltaX;
+        intSlope = Math.round(intSlope * 100) / 100.0;
+        return intSlope;
+    }
+    public double intercept() {
+        b = yOne - (((double) deltaY / deltaX) * xOne);
+        b = Math.round(b * 100) / 100.0;
+        return b;
+    }
+    public double distance()    {
+        double d = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+        d = Math.round(d * 100) / 100.0;
+        return d;
+    }
+    public String lineInfo()  {
+        return"The equation of the line between these slopes is: y = " + slopes() + " + " + intercept() + "\nThe slope of this line is: " + getDoubleSlope() + "\nThe y-intercept of the line is: " + intercept() + "\nThe distance between the two points is: " + distance() ;
+    }
+    public String coordinateForX(double x) {
+        xEquation = x;
+        xEquation *= getDoubleSlope();
+        xEquation += intercept();
+        return "The point on this line is (" + x + ", " + xEquation + ")";
+    }
+
 }
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -14,37 +14,53 @@ public class LinearEquationLogic {
         myScanner = new Scanner(System.in);
     }
     public void start() {
-
+        getData();
     }
-    public void Coords()  {
-        System.out.println("Welc ome to the linear equation calculator! ");
+    private void getData() {
+        System.out.println("Welcome to the linear equation calculator! ");
         String goes = "y";
-        while (goes == "y") {
+        while (goes.equals("y")) {
             System.out.print("Enter first coordinate: ");
             coord1 = myScanner.nextLine();
             System.out.print("Enter second coordinate: ");
             coord2 = myScanner.nextLine();
             System.out.println("");
-            System.out.println("The two points are " + coord1 + "and" + coord2);
-            if (coord1.substring(1,2).equals("-"))  {
-                String strx1 = coord1.substring(1,3);
-                String stry1 = coord1.substring(5);
-            }   else {
-                String strx1 = coord1.substring(1,2);
-                String stry1 = coord1.substring(4);
+            System.out.println("The two points are " + coord1 + " and " + coord2);
+            String strx1;
+            String stry1;
+            String strx2;
+            String stry2;
+            if (coord1.substring(1, 2).equals("-")) {
+                strx1 = coord1.substring(1, 3);
+                stry1 = coord1.substring(5, coord1.length() - 1);
+            } else {
+                strx1 = coord1.substring(1, 2);
+                stry1 = coord1.substring(4, coord1.length() - 1);
             }
-            if (coord2.substring(1,2).equals("-"))  {
-                String strx2 = coord2.substring(1,3);
-                String stry2 = coord2.substring(5);
-            }   else {
-                String strx2 = coord2.substring(1,2);
-                String stry2 = coord2.substring(4);
+            if (coord2.substring(1, 2).equals("-")) {
+                strx2 = coord2.substring(1, 3);
+                stry2 = coord2.substring(5, coord2.length() - 1);
+            } else {
+                strx2 = coord2.substring(1, 2);
+                stry2 = coord2.substring(4, coord2.length() - 1);
             }
+            int x1 = Integer.parseInt(strx1);
+            int y1 = Integer.parseInt(stry1);
+            int x2 = Integer.parseInt(strx2);
+            int y2 = Integer.parseInt(stry2);
 
+            LinearEquation coordy = new LinearEquation(x1, x2, y1, y2);
+            System.out.println(coordy.lineInfo());
+            System.out.println("");
+            System.out.print("Enter a value for x: ");
+            double x = myScanner.nextDouble();
+            myScanner.nextLine();
+            String findY = coordy.coordinateForX(x);
+            System.out.println(findY);
+            System.out.println("");
 
+            System.out.print("Would you like to enter another pair of coordinates? y/n: ");
+            goes = myScanner.nextLine();
         }
-
     }
-
-
 }
